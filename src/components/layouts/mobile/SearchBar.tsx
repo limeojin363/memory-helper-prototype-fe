d8f2c435ccda8cfbf60d92cf9810f02179926faf
Limeojin <2002eojin@gmail.com>
2025-01-19 13:43:26 +0000

    feat: useInput hook 제작과 SearchBar에 의존성 props 추가

--- src/components/layouts/mobile/SearchBar.tsx
+++ src/components/layouts/mobile/SearchBar.tsx
@@ -3,9 +3,15 @@ import S from "./SearchBar.styled";
 
 type SearchBarProps = {
     horizontalPadding?: number;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const SearchBar = ({ horizontalPadding = 16 }: SearchBarProps) => {
+const SearchBar = ({
+    horizontalPadding = 16,
+    value,
+    onChange,
+}: SearchBarProps) => {
     return (
         <S.Outer padding={horizontalPadding}>
             <S.Inner>
@@ -14,7 +20,11 @@ const SearchBar = ({ horizontalPadding = 16 }: SearchBarProps) => {
                     colorName="neutral-dark-dark"
                     size={16}
                 />
-                <S.Input placeholder="Search" />
+                <S.Input
+                    placeholder="Search"
+                    value={value}
+                    onChange={onChange}
+                />
             </S.Inner>
         </S.Outer>
     );
--- src/routes/words.tsx
+++ src/routes/words.tsx
@@ -2,13 +2,16 @@ import { createFileRoute } from "@tanstack/react-router";
 import ProgressBar from "../components/progress-bar/ProgressBar";
 import Header from "../components/layouts/mobile/Header";
 import SearchBar from "../components/layouts/mobile/SearchBar";
+import useInput from "../hooks/useInput";
 
 const RouteComponent = () => {
+    const [value, onChange] = useInput();
+
     return (
         <div>
             <Header title="words" goBack={() => {}} />
             <ProgressBar total={3} current={1} />
-            <SearchBar />
+            <SearchBar value={value} onChange={onChange} />
         </div>
     );
 };
